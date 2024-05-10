f3bfe6f6d41f672540c22db28b0f8531c76d4dbc
drauf727 <153565509+drauf727@users.noreply.github.com>
2024-05-09 23:19:59 -0400

    version 2.0

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
+// Setting up dependencies
 const fs = require('fs');
 const inquirer = require('inquirer');
 const jest = require('jest');
+// Creating a questions list
+const questions = [
+    {
+        name: 'fontcolor',
+        message: 'Type in font color (name or hexadecimal number)',
+        type: 'input'
+    },
+    {
+        name: 'shapecolor',
+        message: "Type in shapes background color (name or hexadecimal number)",
+        type: 'input'
+    },
+    {
+        name: 'shape',
+        message: 'Select shape',
+        type: 'list',
+        choices: ['triangle', 'square', 'circle']
+    }
+]
 
-console.log('Program begin');
-
-console.log('Program end');
+// Inquirer prompt
+inquirer.
+prompt(questions)
+.then((answer) => {
+    console.log(`You selected font color: ${answer.fontcolor}, shape color: ${answer.shapecolor}, and the shape of ${answer.shape}!!`),
+    (err) => err? console.log(err) : console.log('Success');
+})
+.catch((error) =>{
+    console.log(error)
+});
